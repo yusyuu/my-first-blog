103c2ac1a8ab98dc61e44b15ba80bc23f4026659
yusyuu <estrella1227@gmail.com>
2024-03-28 10:22:13 +0900

    git commit -m "add20240328"

---
+++ polls/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.template import loader
+
+from polls.models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    #context = {
+    #    'latest_question_list': latest_question_list,
+    #}
+    #return HttpResponse(template.render(context, request))
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+# Create your views here.
